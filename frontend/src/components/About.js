00aa1e9b2bddf06b14e1a40528c125690c30e432
muhammad waqas <muhammadwaqas.dev99@gmail.com>
2026-06-25 17:47:32 +0500

    feat: initialize NoteContext and add active route highlighting

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      
+        <h1>this is about page</h1>
     </div>
   )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,16 @@ import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import NoteState from './contexts/Note/NoteState';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+  <NoteState>
     <App />
+    </NoteState>
   </BrowserRouter>
 );
 
